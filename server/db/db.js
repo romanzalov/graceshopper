4fd6f0e6036401ca50401c6106a90ca8a1ae902e
mc2147 <matthewchan2147@gmail.com>
2018-04-16 15:51:39 -0500

    3rd commit

--- server/db/db.js
+++ server/db/db.js
@@ -9,4 +9,77 @@ const db = new Sequelize(
     logging: false
   }
 )
+
+const Product = db.define('product', {
+    sportType: {
+      type: Sequelize.ENUM,
+      values: ['Football', 'Basketball', 'Baseball', 'Combat Sports', 'Tennis', 'Soccer', 'Hockey', 'Volleyball', 'Snowboarding', 'Swimming'],
+      allowNull: false,
+    },
+    title: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    quantity: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+    },
+    imageUrl: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    price: {
+      type: Sequelize.DECIMAL,
+      defaultValue: 0,
+    }
+})
+
+const productInstance = db.define('productInstance', {
+  price: {
+    type: Sequelize.DECIMAL,
+    defaultValue: 0,
+  }
+})
+
+//productInstance.belongsTo(Product);
+
+Product.search = function(name) {
+  return Product.findAll(
+    {
+      where: {
+          title:name
+      }
+  })
+}
+
+Product.searchbyCategory = function(category) {
+  return Product.findAll(
+    {
+      where: {
+          sportType:category
+      }
+  })
+}
+
+// const Cart = db.define('cart', {
+
+// })
+
+const User = db.define('user', {
+  email: {
+    type: Sequelize.STRING,
+    unique: true,
+    allowNull: false,
+    validate: {
+      isEmail: true,
+    }
+  },
+  // cart: {
+  //   currentOrder
+  // }
+}) 
+
+// User to product
+  //Purchase history
+
 module.exports = db
