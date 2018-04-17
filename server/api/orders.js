e9216b16ba5675de836001e6658a186f9fe5ebac
Richard Liang <richardliang2015@u.northwestern.edu>
2018-04-17 11:35:44 -0500

    order routes and models

--- server/api/orders.js
+++ server/api/orders.js
@@ -28,59 +28,6 @@ router.put('/:orderId', (req, res, next) => {
 	.catch(next)
 })
 
-//get all orders on specific user
-router.get('/:userId/orders', (req, res, next) => {
-	Order.findAll({
-		where: {
-			userId: req.params.userId,
-		}
-	})
-	.then(orders => res.json(orders))
-	.catch(next)
-})
-
-//get one order on specific user
-router.get('/:userId/orders/:orderId', (req, res, next) => {
-	Order.findOne({
-		where: {
-			userId: req.params.userId,
-			id: req.params.orderId
-		}
-	})
-	.then(orders => res.json(orders))
-	.catch(next)
-})
-
-//delete an order on an user
-router.delete('/:userId/orders/:orderId', (req, res, next) => {
-	Order.destroy({
-		where: {
-			id: req.params.orderId,
-		}
-	})
-	.then(order => res.json(order))
-	.catch(next)
-})
-
-//update an order on an user
-router.put('/:userId/orders/:orderId', (req, res, next) => {
-	Order.update(req.body, {
-		where: {
-			id: req.params.orderId,
-			userId: req.params.userId
-		}
-	})
-	.then(order => res.json(order))
-	.catch(next)
-})
-
-//create an order on an user
-router.post('/:userId/orders', (req, res, next) => {
-	Order.create(req.body)
-	.then(() => res.sendStatus(201))
-	.catch(next)
-})
-
 
 router.use((req, res, next) => {
   const error = new Error('Not Found')
