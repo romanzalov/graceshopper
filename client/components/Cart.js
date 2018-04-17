7b9593a48e1e6898bf1e5dbb480b8332917b380e
Richard Liang <richardliang2015@u.northwestern.edu>
2018-04-17 16:38:51 -0500

    added react files

---
+++ client/components/Cart.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+import {connect} from 'react-redux'
+import {Link} from 'react-router-dom'
+
+class Cart extends Component {
+	constructor(props) {
+		super(props);
+	}
+
+	render() {
+		return (
+			<div></div>
+
+			)
+	}
+}
+
+const mapStateToProps = function(state) {
+	
+}
+
+const mapDispatchToProps = function(dispatch) {
+
+}
+
+export default connect(mapStateToProps,mapDispatchToProps)(Cart)
